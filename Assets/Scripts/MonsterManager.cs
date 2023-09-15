081456c8ac234c55e0e6655e2ac09a4b0e3d7278
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-09-15 11:29:40 +0300

    enemy

--- Assets/Scripts/MonsterManager.cs
+++ Assets/Scripts/MonsterManager.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
 {
+    public float spawnXRight, spawnXLeft, spawnY;
+
+    [Space(10)]
+
     public GameObject monsterPrefab;
 
-    public Vector2 monsterPos1;
-    public Vector2 monsterPos2;
+    float spawnIntervalLeft;
+    float spawnIntervalRight;
+
+    private GameObject leftMonster, rightMonster;
+    private bool isSpawningFromFirstPos = true; // Flag to alternate spawn positions
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        spawnIntervalLeft = Random.Range(0, 1);
+        spawnIntervalRight = Random.Range(0, 1);
 
-    // Update is called once per frame
-    void Update()
+        StartCoroutine(SpawnLeftMonster());
+        StartCoroutine(SpawnRightMonster());
+    }
+    private IEnumerator SpawnLeftMonster()
     {
-        
+        while (true)
+        {
+            // Spawn a monster on the left side
+            leftMonster = Instantiate(monsterPrefab, SpawnPosition(spawnXLeft, spawnY), Quaternion.identity);
+
+            // Wait for the current left-side monster to disappear
+            yield return new WaitUntil(() => leftMonster == null);
+
+            // Wait for the specified interval before spawning the next left-side monster
+            yield return new WaitForSeconds(spawnIntervalLeft);
+        }
+    }
+    private IEnumerator SpawnRightMonster()
+    {
+        while (true)
+        {
+            // Spawn a monster on the right side
+            rightMonster = Instantiate(monsterPrefab, SpawnPosition(spawnXRight, spawnY), Quaternion.identity);
+
+            // Wait for the current right-side monster to disappear
+            yield return new WaitUntil(() => rightMonster == null);
+
+            // Wait for the specified interval before spawning the next right-side monster
+            yield return new WaitForSeconds(spawnIntervalRight);
+        }
+    }
+    private Vector2 SpawnPosition(float x, float y)
+    {
+        return new Vector2(x, y);
     }
 }
